5d60bc099baf900eb744324cd82bfb7d98227e74
José Cuentas <elapatico123@gmail.com>
2021-03-08 17:28:49 -0500

    Completando la hackathon #13

--- js/app.js
+++ js/app.js
@@ -2,8 +2,13 @@ let contenedorMascotas = document.querySelector('.contenedor-mascotas');
 let agregarMascotaBtn = document.querySelector('.agregar-mascota');
 let contenedorAgregar = document.querySelector('.contenedor-agregar');
 let confirmarAgregarBtn = document.querySelector('.confirmar-agregar');
+let cerrarContenedorAgregar = document.querySelector('.cerrar-agregar');
+let contenedorActualizar = document.querySelector('.contenedor-actualizar');
+let cerrarContenedorActualizar = document.querySelector('.cerrar-actualizar');
+let confirmarActualizarBtn = document.querySelector('.confirmar-actualizar');
+let objeto={};
 let mascotas=[];
-let contador = 8;
+let contador = 9;
 
 capturarDatos();
 
@@ -15,7 +20,7 @@ function capturarDatos(){
 
     xhr.onload = function (){
         mascotas = JSON.parse(this.responseText);
-        llenarDatos(mascotas);
+        llenarDatos(mascotas,contenedorMascotas);
     
     }
 
@@ -23,13 +28,13 @@ function capturarDatos(){
 }
 
 
-function llenarDatos(Cmascotas){
-    Cmascotas.forEach(mascota => {
-        contenedorMascotas.insertAdjacentHTML('beforeend',`
+function llenarDatos(Rmascotas, Rcontenedor){
+    Rmascotas.forEach(mascota => {
+        Rcontenedor.insertAdjacentHTML('beforeend',`
         
         <article class="contenedor-mascota" id="${mascota.ID}">
             <header>
-                <button class="editar">Edit</button>
+                <button class="actualizar">Edit</button>
                 <button class="eliminar">Delt</button>
             </header>
             <img src="${mascota.Foto}" alt="" class="foto-mascota">
@@ -49,6 +54,7 @@ agregarMascotaBtn.addEventListener('click', (e) =>{
     e.preventDefault();
 
     contenedorAgregar.classList.toggle('contenedor-agregar-active');
+    contenedorMascotas.classList.toggle('contenedor-mascotas-active');
 
 });
 
@@ -58,16 +64,94 @@ confirmarAgregarBtn.addEventListener('click', (e)=>{
         "Nombre": document.getElementById('agregar-nombre').value,
         "Telefono": document.getElementById('agregar-telefono').value,
         "Email": document.getElementById('agregar-email').value,
-        "Foto": document.getElementById('agregar-foto').value,
+        // "Foto": document.getElementById('agregar-foto').value,
+        "Foto": "images/mascota1.jpg",
         "Pais": document.getElementById('agregar-pais').value,
         "Descripcion": document.getElementById('agregar-descripcion').value,
         "ID": contador+1
     }
-    if(mascotas.push(nuevaMascota)){
-        llenarDatos(mascotas);
-        alert(`Mascota agregada`);
-    }else{
-        console.log(`Error`);
+
+    
+    insertarYMostrar(mascotas,nuevaMascota,contenedorMascotas);
+    contenedorAgregar.classList.toggle('contenedor-agregar-active');
+    contenedorMascotas.classList.toggle('contenedor-mascotas-active');
+
+    
+});
+
+function limpiarContenedor(Rcontenedor){
+    Rcontenedor.innerHTML='';
+}
+
+cerrarContenedorAgregar.addEventListener('click', (e)=>{
+    e.preventDefault();
+
+    contenedorAgregar.classList.toggle('contenedor-agregar-active');
+    contenedorMascotas.classList.toggle('contenedor-mascotas-active');
+
+});
+
+function insertarYMostrar(Rmascotas, RnuevaMascota,Rcontenedor){
+    Rmascotas.push(RnuevaMascota);
+    limpiarContenedor(Rcontenedor);
+    llenarDatos(Rmascotas,Rcontenedor);
+}
+
+function insertarYMostrarNuevo(Rmascotas,Rcontenedor){
+    limpiarContenedor(Rcontenedor);
+    llenarDatos(Rmascotas,Rcontenedor);
+}
+
+contenedorMascotas.addEventListener('click', (e)=>{
+    e.preventDefault();
+    let elemento;
+
+    if(e.target.classList.contains('eliminar')){
+        elemento=e.target.parentElement.parentElement;
+        mascotas=mascotas.filter(mascota => mascota.ID!==parseInt(elemento.getAttribute('id')));
+        insertarYMostrarNuevo(mascotas,contenedorMascotas);
     }
     
+    if(e.target.classList.contains('actualizar')){
+        elemento=e.target.parentElement.parentElement;
+        CajaActualizarInicial(elemento,mascotas);
+        contenedorActualizar.classList.toggle('contenedor-actualizar-active');
+        contenedorMascotas.classList.toggle('contenedor-mascotas-active');
+    }
+
+});
+
+
+cerrarContenedorActualizar.addEventListener('click', (e)=>{
+    e.preventDefault();
+
+    contenedorActualizar.classList.toggle('contenedor-actualizar-active');
+    contenedorMascotas.classList.toggle('contenedor-mascotas-active');
+
+});
+
+function CajaActualizarInicial(Relemento,Rmascotas){
+    
+    contenedorActualizar.querySelector('#actualizar-nombre').value=Relemento.querySelector('.nombre').textContent;
+    contenedorActualizar.querySelector('#actualizar-pais').value=Relemento.querySelector('.pais').textContent;
+    contenedorActualizar.querySelector('#actualizar-telefono').value=Relemento.querySelector('.telefono').textContent;
+    contenedorActualizar.querySelector('#actualizar-email').value=Relemento.querySelector('.email').textContent;
+    contenedorActualizar.querySelector('#actualizar-descripcion').value=Relemento.querySelector('.descripcion').textContent;
+
+    objeto=Rmascotas.find(mascota => mascota.ID == Relemento.getAttribute('id'));
+
+}
+
+confirmarActualizarBtn.addEventListener('click', () =>{
+
+    objeto.Nombre= contenedorActualizar.querySelector('#actualizar-nombre').value;
+    objeto.Telefono= contenedorActualizar.querySelector('#actualizar-telefono').value;
+    objeto.Email= contenedorActualizar.querySelector('#actualizar-email').value;
+    objeto.Pais= contenedorActualizar.querySelector('#actualizar-pais').value;
+    objeto.Descripcion= contenedorActualizar.querySelector('#actualizar-descripcion').value;
+    
+    insertarYMostrarNuevo(mascotas, contenedorMascotas);
+    objeto={};
+    contenedorActualizar.classList.toggle('contenedor-actualizar-active');
+    contenedorMascotas.classList.toggle('contenedor-mascotas-active');
 });
